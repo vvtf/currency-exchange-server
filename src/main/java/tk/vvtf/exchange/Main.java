d092596607d5b6d8d5d3dafb2763d020793b6f05
Dominik Labuda <dominikla@gmail.com>
2019-04-28 23:02:05 +0200

    Initial java setup

---
+++ src/main/java/tk/vvtf/exchange/Main.java
@@ -0,0 +1,11 @@
+package tk.vvtf.exchange;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String... args) {
+        System.out.println(Arrays.toString(args));
+    }
+
+}
